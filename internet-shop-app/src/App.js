e69726980e864891f917e2979be4b3f1021d7a7e
Egor_Trubilo <trubilooficial@gmail.com>
2021-12-29 22:31:03 +0300

    5

--- internet-shop-app/src/App.js
+++ internet-shop-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
             </div>
             <div className="header__cart">
               <a href="/cart.html" className="button button--cart">
-                <span>520 ₽ </span>
+                <span>520 ₽      </span>
 
                 <div className="button__delimiter"></div>
                 <svg
